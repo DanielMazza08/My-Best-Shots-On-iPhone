37b05bcfc34bd179fa9b2d9823a3bdef73e13f4b
Cristian Daniel Mazza <cristianmazza08@gmail.com>
2022-05-09 11:59:38 -0300

    Agregado modal de Contacto

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/* Modal para la galeria */
+
+        var fullImgBox = document.getElementById("fullImgBox");
+        var fullImg = document.getElementById("fullImg");
+
+        function openFullImg(pic){
+            fullImgBox.style.display = "flex";
+            fullImg.src = pic;
+        }
+
+        function closeFullImg(){
+            fullImgBox.style.display = "none";
+        }
+
+/* Modal para Contacto */
+
+
+
+/* Cambiar de modo claro a modo oscuro */
